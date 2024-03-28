9f1861349d5449a903a469e4a3c50a5e59d25c75
mohammedyh <mohammedyousufhamid@gmail.com>
2024-03-28 21:03:00 +0000

    feat: convert express API to ES modules
    Switch package manager from npm to pnpm

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import fetch from 'node-fetch';
-import path from 'path';
+import path from 'node:path';
 import dotenv from 'dotenv';
 
-const app = express();
 dotenv.config();
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve(path.dirname(''));
@@ -24,4 +23,4 @@ app.get('/user/:userName', async (req, res) => {
 	res.status(200).json(data);
 });
 
-app.listen(PORT, () => console.log(`server running on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`));
