9786a40ca49e2ac9b106420b1510577c6da80bfb
mohammedyh <mohammedyousufhamid@gmail.com>
2024-10-10 00:45:22 +0100

    refactor: client-side data fetching and error handling
    adjust code formatting

--- public/main.js
+++ public/main.js
@@ -3,26 +3,12 @@ const errorWrapper = document.querySelector('.error__wrapper');
 const btn = document.querySelector('#search-btn');
 const input = document.querySelector('input');
 
-const getUserData = async () => {
-	const userInput = input.value.trim();
+const getUserData = async userName => {
 	try {
-		const response = await fetch(`/user/${userInput}`);
-
-		if (!response.ok) {
-			throw new Error(`HTTP ${response.status} - ${response.statusText}`);
-		}
-
+		const response = await fetch(`/users/${userName}`);
 		const data = await response.json();
 
-		if (data?.error) {
-			throw new Error(data.message);
-		}
-
-		if (data?.message == 'Not Found') {
-			throw new Error(
-				'No account was found with the provided username, please try again'
-			);
-		}
+		if (data?.error) throw Error(data.message);
 
 		input.value = '';
 		return { data };
@@ -33,13 +19,18 @@ const getUserData = async () => {
 
 const showUserInfo = async e => {
 	e.preventDefault();
+	const userName = input.value.trim();
 
 	try {
-		const res = await getUserData();
-
-		if (!res.data) {
-			throw new Error(res);
+		if (!userName) {
+			input.value = '';
+			input.focus();
+			throw Error('Username cannot be empty');
 		}
+
+		const res = await getUserData(userName);
+
+		if (!res.data) throw Error(res);
 
 		userInfoWrapper.innerHTML = `
          <div class="w-5/6 lg:max-w-4xl p-6 md:p-10 mx-auto flex flex-col items-center md:flex-row md:items-start bg-slate-800 rounded-xl shadow-lg">
@@ -53,9 +44,9 @@ const showUserInfo = async e => {
 
            <div class="user__info w-full mt-10 md:mt-4 md:ml-10">
              <div class="flex items-center md:items-start flex-col lg:flex-row lg:justify-between">
-               <h2 class="basis-2/3 lg:basis-auto text-2xl lg:text-3xl font-bold text-white">${
-									res.data.name ?? res.data.login
-								}</h2>
+               <h2 class="basis-2/3 lg:basis-auto text-2xl lg:text-3xl font-bold text-white">
+                 ${res.data.name ?? res.data.login}
+               </h2>
                <p class="basis-1/3 lg:basis-auto mt-2 md:mt-0 text-sm sm:text-base lg:text-lg text-gray-400">
                  Joined ${new Date(res.data.created_at).toDateString()}
                </p>
@@ -74,23 +65,23 @@ const showUserInfo = async e => {
              <div class="max-w-full mx-auto flex justify-between p-6 lg:p-8 mt-6 bg-slate-900 rounded-xl">
                <div>
                  <p class="text-gray-400 text-sm sm:text-base lg:text-lg">Repos</p>
-                 <p class="text-white text-lg sm:text-xl lg:text-2xl font-bold">${
-										res.data.public_repos
-									}</p>
+                 <p class="text-white text-lg sm:text-xl lg:text-2xl font-bold">
+                   ${res.data.public_repos}
+                 </p>
                </div>
 
                <div>
                  <p class="text-gray-400 text-sm sm:text-base lg:text-lg">Followers</p>
-                 <p class="text-white text-lg sm:text-xl lg:text-2xl font-bold">${
-										res.data.followers
-									}</p>
+                 <p class="text-white text-lg sm:text-xl lg:text-2xl font-bold">
+                   ${res.data.followers}
+                 </p>
                </div>
 
                <div>
                  <p class="text-gray-400 text-sm sm:text-base lg:text-lg">Following</p>
-                 <p class="text-white text-lg sm:text-xl lg:text-2xl font-bold">${
-										res.data.following
-									}</p>
+                 <p class="text-white text-lg sm:text-xl lg:text-2xl font-bold">
+                   ${res.data.following}
+                 </p>
                </div>
              </div>
 
@@ -101,9 +92,9 @@ const showUserInfo = async e => {
                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                  </svg>
 
-                 <p class="ml-2 text-gray-400 text-sm lg:text-lg">${
-										res.data.location ?? 'No location'
-									}</p>
+                 <p class="ml-2 text-gray-400 text-sm lg:text-lg">
+                   ${res.data.location ?? 'No location'}
+                 </p>
                </div>
 
                <div class="flex items-center">
@@ -137,9 +128,9 @@ const showUserInfo = async e => {
                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                  </svg>
 
-                 <p class="ml-2 text-gray-400 text-sm lg:text-lg">${
-										res.data.email ?? 'No email'
-									}</p>
+                 <p class="ml-2 text-gray-400 text-sm lg:text-lg">
+                   ${res.data.email ?? 'No email'}
+                 </p>
                </div>
 
                <div class="flex items-center">
@@ -147,9 +138,9 @@ const showUserInfo = async e => {
                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
                  </svg>
 
-                 <p class="ml-2 text-gray-400 text-sm lg:text-lg">${
-										res.data.company ?? 'No company'
-									}</p>
+                 <p class="ml-2 text-gray-400 text-sm lg:text-lg">
+                   ${res.data.company ?? 'No company'}
+                 </p>
                </div>
              </div>
            </div>
