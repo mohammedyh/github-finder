dc06970e574c40a204f8c9836e1c48f6f72fbe1e
mohammedyh <mohammedyousufhamid@gmail.com>
2024-10-10 08:59:39 +0100

    fix(debug): comment out code to troubleshoot deployment

--- src/server.ts
+++ src/server.ts
@@ -13,36 +13,34 @@ app.get('/users/:userName', validateUsernameParam, async c => {
 	const { userName } = c.req.valid('param');
 
 	try {
-		const userData = await fetch(
-			`https://api.github.com/users/${userName}`
-		);
+		const userData = await fetch(`https://api.github.com/users/${userName}`);
 
-		if (!userData.ok) {
-			if (userData.status == 404) {
-				return c.json(
-					{ error: true, message: `The user ${userName} doesn't exist.` },
-					404
-				);
-			}
+		// if (!userData.ok) {
+		// 	if (userData.status == 404) {
+		// 		return c.json(
+		// 			{ error: true, message: `The user ${userName} doesn't exist.` },
+		// 			404
+		// 		);
+		// 	}
 
-			return c.json(
-				{
-					error: true,
-					message: 'There was an error processing your request.',
-				},
-				500
-			);
-		}
+		// 	return c.json(
+		// 		{
+		// 			error: true,
+		// 			message: 'There was an error processing your request.',
+		// 		},
+		// 		500
+		// 	);
+		// }
 
-		if (userData.headers.get('x-ratelimit-remaining') == '0') {
-			return c.json(
-				{
-					error: true,
-					message: 'API rate limit exceeded. No further requests allowed.',
-				},
-				429
-			);
-		}
+		// if (userData.headers.get('x-ratelimit-remaining') == '0') {
+		// 	return c.json(
+		// 		{
+		// 			error: true,
+		// 			message: 'API rate limit exceeded. No further requests allowed.',
+		// 		},
+		// 		429
+		// 	);
+		// }
 
 		const data = await userData.json();
 		return c.json(data);
